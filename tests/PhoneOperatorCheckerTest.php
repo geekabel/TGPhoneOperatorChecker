2deb67928b5d215e61125163c869856547bc852a
Godwin K <koffigodwin96@gmail.Com>
2022-10-17 16:48:18 +0200

    Test:first test

--- tests/PhoneOperatorCheckerTest.php
+++ tests/PhoneOperatorCheckerTest.php
@@ -2,6 +2,11 @@
 
 namespace Godwin\TgPhoneOperatorCheckerTest;
 
-class PhoneOperatorCheckerTest {
+use PHPUnit\Framework\TestCase;
 
+class PhoneOperatorCheckerTest extends TestCase {
+
+    public function testCanReturnRightOPerator() {
+        $this->assertEquals(4, 2 + 2);
+    }
 }
